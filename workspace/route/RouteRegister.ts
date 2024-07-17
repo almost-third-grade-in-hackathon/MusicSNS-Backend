3aee6b09d16198b594bbc6b89e787db891244eaa
suisan0731 <suisan0731@gmail.com>
2024-07-18 01:58:36 +0900

    routeregisterの修正前差分をコメントとしてこのしておいた

--- workspace/route/RouteRegister.ts
+++ workspace/route/RouteRegister.ts
@@ -15,3 +15,18 @@ export default function RouteRegister<T, QueryString>({fastify,route,method}:{fa
         throw new Error("想定していないデータが入りました")
     }
 }
+
+/*
+念のため修正前差分
+export default function RouteRegister({fastify,route,method}:{fastify:FastifyInstance ,route: RouteType,method: "GET" | "POST" | "DELETE"},func: RouteHandlerMethod) {
+    if(method === "GET") {
+        fastify.get(route,func)
+    }else if(method === "POST") {
+        fastify.post(route,func)
+    } else if(method === "DELETE") {
+        fastify.delete(route,func)
+    } else {
+        throw new Error("想定していないデータが入りました")
+    }
+}
+*/
