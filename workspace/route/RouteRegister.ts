53264e85f8a2c22cad0fe08668cb7efb2bc59c80
suisan0731 <suisan0731@gmail.com>
2024-07-18 01:54:26 +0900

    routeregisterを型定義出来るよう修正したけど多分間違っている

--- workspace/route/RouteRegister.ts
+++ workspace/route/RouteRegister.ts
@@ -2,13 +2,15 @@ import type { FastifyInstance, RouteHandlerMethod } from "fastify";
 import type { RouteType } from "./RouteType";
 import type RouteHelper from "./RouteType";
 
-export default function RouteRegister({fastify,route,method}:{fastify:FastifyInstance ,route: RouteType,method: "GET" | "POST" | "DELETE"},func: RouteHandlerMethod) {
+export default function RouteRegister<T, QueryString>({fastify,route,method}:{fastify:FastifyInstance ,route: RouteType,method: "GET" | "POST" | "DELETE"| "PUT"},func: RouteHandlerMethod) {
     if(method === "GET") {
-        fastify.get(route,func)
+        fastify.get<{Reply: T}>(route,func)
     }else if(method === "POST") {
-        fastify.post(route,func)
+        fastify.post<{Body: T, QueryString: QueryString}>(route,func)
     } else if(method === "DELETE") {
         fastify.delete(route,func)
+    } else if(method === "PUT") {
+        fastify.put<{Body: T, QueryString: QueryString}>(route,func)
     } else {
         throw new Error("想定していないデータが入りました")
     }
--- workspace/route/post/index.ts
+++ workspace/route/post/index.ts
@@ -1,7 +1,15 @@
 import FastifyPlugin from "fastify-plugin"
+import RouteRegister from "../RouteRegister"
+import RouteHelper from "../RouteType"
 
 export default FastifyPlugin(async function(fastify,opt){
-    
+    /*
+    途中
+    RouteRegister(
+        {fastify, route: RouteHelper({root: "post", end: []}), method: "GET"},
+        async (request, reply)
+    )
+    */
 })
 
 
