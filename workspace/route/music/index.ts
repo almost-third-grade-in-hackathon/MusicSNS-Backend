68b62c9ea082808c9d90ba68dee014a40cdd3820
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-07-17 22:52:23 +0900

    より使いやすいようにRouteHelperの引数と処理内容を変更、Spotify APIとの連携を作成開始

--- workspace/route/music/index.ts
+++ workspace/route/music/index.ts
@@ -1,7 +1,22 @@
 import FastifyPlugin from "fastify-plugin"
+import {SpotifyApi} from "@spotify/web-api-ts-sdk"
+import RouteRegister from "../RouteRegister"
+import RouteHelper from "../RouteType"
+
+const sdk = SpotifyApi.withUserAuthorization(process.env.SPOTIFY_CLIENT_ID as string,"http://localhost:8000")
 
 export default FastifyPlugin(async function(fastify,opt){
-
+    RouteRegister(
+        {fastify,route: RouteHelper({root: "music",end: ["track","search"]}),method:"POST"},
+        async (request,reply) => {
+            const result = sdk.search("I'm Fading Away" as string,["track"])
+            reply.type("application/json")
+            reply.send({
+                tracks: (await result).tracks.items
+            })
+        }
+    )
+    RouteRegister
 })
 
 const GetMusic = () => {
