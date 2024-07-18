848e11bd6aa0e2003116e47a2000f2f6fa587001
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-07-18 22:35:28 +0900

    型引数をわかりやすく

--- workspace/route/RouteRegister.ts
+++ workspace/route/RouteRegister.ts
@@ -2,15 +2,15 @@ import type { FastifyInstance, RouteHandlerMethod } from "fastify";
 import type { RouteType } from "./RouteType";
 import type RouteHelper from "./RouteType";
 
-export default function RouteRegister<T, QueryString>({fastify,route,method}:{fastify:FastifyInstance ,route: RouteType,method: "GET" | "POST" | "DELETE"| "PUT"},func: RouteHandlerMethod) {
+export default function RouteRegister<B, Q extends Record<string,string>,R extends Record<string,string>>({fastify,route,method}:{fastify:FastifyInstance ,route: RouteType,method: "GET" | "POST" | "DELETE"| "PUT"},func: RouteHandlerMethod) {
     if(method === "GET") {
-        fastify.get<{Reply: T}>(route,func)
+        fastify.get<{Reply: R}>(route,func)
     }else if(method === "POST") {
-        fastify.post<{Body: T, QueryString: QueryString}>(route,func)
+        fastify.post<{Body: B, QueryString: Q}>(route,func)
     } else if(method === "DELETE") {
         fastify.delete(route,func)
     } else if(method === "PUT") {
-        fastify.put<{Body: T, QueryString: QueryString}>(route,func)
+        fastify.put<{Body: B, QueryString: Q}>(route,func)
     } else {
         throw new Error("想定していないデータが入りました")
     }
