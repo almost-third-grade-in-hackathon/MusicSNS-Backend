1f2683d1bac9bc56e67f8273ed90df76e5e469af
suisan0731 <suisan0731@gmail.com>
2024-07-18 22:37:05 +0900

    Postの部分途中経過

--- workspace/convex/post.ts
+++ workspace/convex/post.ts
@@ -31,7 +31,7 @@ export const createPost = mutation({
     },
     handler: async (ctx: any, args: any) => {
         /* dbにPOST */
-        const tasks = await ctx.db.insert("post", {
+        await ctx.db.insert("post", {
             user_id: args.user_id,
             contents: args.contents,
             playlist_id: args.playlist_id, //ここどうするか
@@ -39,13 +39,42 @@ export const createPost = mutation({
             likes: args.likes,
             comments: args.comments
         })
+        return {status: "POST successed."}
     }
 })
+
+/* 投稿更新 */
+export const updatePost = mutation({
+    /* handlerに渡す引数を指定 (要修正)*/
+    args: {
+        id: v.id("post"),
+        user_id: v.id("users"),
+        contents: v.string(),
+        playlist_id: v.optional(v.id("playlist")), //ここどうするか
+        music_id: v.optional(v.string()),   //ここどうするか
+        likes: v.number(),
+        comments: v.array(v.id("comment"))
+    },
+    handler: async (ctx: any, args: any) => {
+        /* dbにPUT */
+        await ctx.db.replace(args.id, {
+            user_id: args.user_id,
+            contents: args.contents,
+            playlist_id: args.playlist_id, //ここどうするか
+            music_id: args.music_id, //ここどうするか
+            likes: args.likes,
+            comments: args.comments
+        })
+        return {status: "PUT successed."}
+    }
+})
+
 
 /* 記事削除　*/
 export const deletePost = mutation({
     args: {id: v.id("post")}, //postテーブルのドキュメントIDを指定 (合ってるかわからんです)
     handler: async (ctx: any, args: any) => {
         await ctx.db.delete(args.id);   //削除したい投稿のidを指定し投稿記事削除
+        return {status: "DELETE successed."}
     }
 })
--- workspace/route/post/index.ts
+++ workspace/route/post/index.ts
@@ -1,15 +1,43 @@
 import FastifyPlugin from "fastify-plugin"
 import RouteRegister from "../RouteRegister"
 import RouteHelper from "../RouteType"
+import { useQuery, useMutation } from "convex/react";
+import { api } from "../convex/_generated/api";
 
 export default FastifyPlugin(async function(fastify,opt){
-    /*
-    途中
+    /* POSTテーブルからGET (クエリパラメータを用いた条件検索は未実装)*/
     RouteRegister(
         {fastify, route: RouteHelper({root: "post", end: []}), method: "GET"},
-        async (request, reply)
+        async (request, reply) => {
+            const response_data = useQuery(api.post.getPost, {});
+            reply.type("application/json")
+            reply.send(response_data)
+        }
     )
-    */
+
+    RouteRegister(
+        
+
+        {fastify, route: RouteHelper({root: "post", end: []}), method: "POST"},
+        async (request, reply) => {
+            request.body
+            const response_status = useMutation(api.post.postPost, {});
+        }
+    )
+
+    RouteRegister(
+        {fastify, route: RouteHelper({root: "post", end: []}), method: "PUT"},
+        async (request, reply) => {
+            const response_data = useMutation(api.post.createPost, {});
+        }
+    )
+
+    RouteRegister(
+        {fastify, route: RouteHelper({root: "post", end: []}), method: "DELETE"},
+        async (request, reply) => {
+            const response_data = useMutation(api.post.deletePost, {});
+        }
+    )
 })
 
 
