79cff2379cc3a87aa44bad6012d95e7f90fe5aca
suisan0731 <suisan0731@gmail.com>
2024-07-20 04:16:04 +0900

    post apiとクエリとりあえずここまでコミット

--- workspace/convex/post.ts
+++ workspace/convex/post.ts
@@ -1,3 +1,4 @@
+
 import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 
@@ -18,6 +19,19 @@ export const getPost = query({
     }
 })
 
+/* 投稿記事検索　(未実装)*/
+export const searchPost = query({
+    /* handlerに渡す引数を指定 (要修正)*/
+    args: {q: v.string()},
+    handler: async (ctx: any, args: any) => {
+        const tasks = await ctx.db
+            .query("post")      //postテーブルのデータを指定
+            .order("desc")      //最新データが先頭に来るようソート指定
+            .take(10)       //10件取得 代わりに.collect()メソッドでテーブルの全データを指定可能
+        return tasks
+    }
+})
+
 /* 投稿 */
 export const createPost = mutation({
     /* handlerに渡す引数を指定 (要修正)*/
@@ -26,20 +40,18 @@ export const createPost = mutation({
         contents: v.string(),
         playlist_id: v.optional(v.id("playlist")), //ここどうするか
         music_id: v.optional(v.string()),   //ここどうするか
-        likes: v.number(),
-        comments: v.array(v.id("comment"))
     },
     handler: async (ctx: any, args: any) => {
         /* dbにPOST */
-        await ctx.db.insert("post", {
+        const tasks = await ctx.db.insert("post", {
             user_id: args.user_id,
             contents: args.contents,
             playlist_id: args.playlist_id, //ここどうするか
             music_id: args.music_id, //ここどうするか
-            likes: args.likes,
-            comments: args.comments
+            likes: 0,
+            comments: []
         })
-        return {status: "POST successed."}
+        return tasks
     }
 })
 
@@ -48,24 +60,18 @@ export const updatePost = mutation({
     /* handlerに渡す引数を指定 (要修正)*/
     args: {
         id: v.id("post"),
-        user_id: v.id("users"),
         contents: v.string(),
         playlist_id: v.optional(v.id("playlist")), //ここどうするか
         music_id: v.optional(v.string()),   //ここどうするか
-        likes: v.number(),
-        comments: v.array(v.id("comment"))
     },
     handler: async (ctx: any, args: any) => {
         /* dbにPUT */
-        await ctx.db.replace(args.id, {
-            user_id: args.user_id,
+        const tasks = await ctx.db.replace(args.id, {
             contents: args.contents,
             playlist_id: args.playlist_id, //ここどうするか
             music_id: args.music_id, //ここどうするか
-            likes: args.likes,
-            comments: args.comments
         })
-        return {status: "PUT successed."}
+        return tasks
     }
 })
 
@@ -74,7 +80,7 @@ export const updatePost = mutation({
 export const deletePost = mutation({
     args: {id: v.id("post")}, //postテーブルのドキュメントIDを指定 (合ってるかわからんです)
     handler: async (ctx: any, args: any) => {
-        await ctx.db.delete(args.id);   //削除したい投稿のidを指定し投稿記事削除
-        return {status: "DELETE successed."}
+        const tasks = await ctx.db.delete(args.id);   //削除したい投稿のidを指定し投稿記事削除
+        return tasks
     }
 })
--- workspace/route/post/index.ts
+++ workspace/route/post/index.ts
@@ -1,31 +1,118 @@
 import FastifyPlugin from "fastify-plugin"
 import { api } from "../../convex/_generated/api";
 import ConvexSubscribeClient from "../ConvexSubscribeClient";
+import RouteRegister from "../RouteRegister"
+import RouteHelper from "../RouteType"
+import type { Doc } from "../../convex/_generated/dataModel"
 
 export default FastifyPlugin(async function(fastify,opt){
-    /* POSTテーブルからGET (クエリパラメータを用いた条件検索は未実装)*/
+    /* 記事取得　*/
+    RouteRegister<{}, {}, {
+        post: Doc<"post">
+    }>(
+        {fastify,route: RouteHelper({root: "post",end: []}),method:"GET"},
+        async (request,reply) => {
+            const res = await ConvexSubscribeClient.query(api.post.getPost, {});
+            reply.type("application/json")
+            .send({ post: res });
+        }
+    )
+
+    /* 記事検索　*/
+    RouteRegister<{
+        q: string
+    }, {}, {
+        post: Doc<"post">
+    }>(
+        {fastify,route: RouteHelper({root: "post",end: []}),method:"GET"},
+        async (request,reply) => {
+            const res = await ConvexSubscribeClient.query(api.post.searchPost, {q: request.query.q});
+            reply.type("application/json")
+            .send({ post: res });
+        }
+    )
+
+    /* 記事を投稿 */
+    RouteRegister<{
+        user_id: string,
+        contents: string,
+        playlist_id: string,
+        music_id: string,
+    }, {}, {
+        post: Doc<"post">
+    }>(
+        {fastify,route: RouteHelper({root: "post",end: []}),method:"POST"},
+        async (request,reply) => {
+            const res = await ConvexSubscribeClient.mutation(api.post.createPost, {
+                user_id: request.query.user_id,
+                contents: request.query.contents,
+                playlist_id: request.query.playlist_id,
+                music_id: request.query.music_id,
+            });
+            reply.type("application/json")
+            reply.send({ post: res });
+        }
+    )
+
+    /* 記事を更新 */
+    RouteRegister<{
+        id: Doc<"post">["_id"],
+        contents: string,
+        playlist_id: string,
+        music_id: string,
+    }, {}, {
+        post: Doc<"post">
+    }>(
+        {fastify,route: RouteHelper({root: "post",end: []}),method:"PUT"},
+        async (request,reply) => {
+            const res = await ConvexSubscribeClient.mutation(api.post.updatePost, {
+                id: request.query.id,
+                contents: request.query.contents,
+                playlist_id: request.query.playlist_id,
+                music_id: request.query.music_id
+            });
+            reply.type("application/json")
+            reply.send({ post: res });
+        }
+    )
+
+    /* 記事削除 */
+    RouteRegister<{
+        id: Doc<"post">["_id"]
+    }, {}, {
+        post: Doc<"post">
+    }>(
+        {fastify,route: RouteHelper({root: "post",end: []}),method:"DELETE"},
+        async (request,reply) => {
+            const res = await ConvexSubscribeClient.mutation(api.post.deletePost, {
+                id: request.query.id
+            });
+            reply.type("application/json")
+            reply.send({post: res});
+        }
+    )
+
+
+    /*
     fastify.get("/post", async (request, reply) => {
         const res = await ConvexSubscribeClient.query(api.post.getPost, {});
         reply.type("application/json")
         reply.send(res);
     })
 
-    /* 記事を投稿 */
     fastify.post("/post", async (request, reply) => {
-        console.log(request.body)
         const res = await ConvexSubscribeClient.mutation(api.post.createPost, {});
         reply.type("application/json").send(res);
     })
-
-    /* 記事を更新 */
+    
     fastify.put("/post", async (request, reply) => {
         const res = await ConvexSubscribeClient.mutation(api.post.updatePost, {});
         reply.type("application/json").send(res);
     })
-
-    /* 記事削除 */
+    
     fastify.delete("/post", async (request, reply) => {
         const res = await ConvexSubscribeClient.mutation(api.post.deletePost, {});
         reply.type("application/json").send(res);
     })
+    */
 })
